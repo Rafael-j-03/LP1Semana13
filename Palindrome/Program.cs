5511e37f1f6df84ac1469398deccd8660f70ac76
Rafael José <ra.david@hotmail.com>
2023-06-08 01:32:16 +0100

    Add local function to CheckPalindrome

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -6,18 +6,41 @@ namespace Palindrome
     {
         static void Main(string[] args)
         {
-            
+            for (int i = 0; i < args.Length; ++i)
+            {
+                Console.WriteLine($"{args[i]} -> {isPalindrome(args[i])}");
+            }
         }
 
-        private bool isPalindrome(string word)
+        public static bool isPalindrome(string word)
         {
             if (word == null)
             {
-                throw new ArgumentNullException(nameof(word));
+                throw new ArgumentNullException();
             }
             else if (word.Length < 2)
             {
-                return true
+                return true;
+            }
+            else
+            {
+                return CheckPalindrome(word);
+            }
+
+            bool CheckPalindrome(string word)
+            {
+                char[] charArray = word.ToCharArray();
+                Array.Reverse(charArray);
+                string reversedWord = new string(charArray);
+
+                if (word == reversedWord)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
             }
         }
     }
